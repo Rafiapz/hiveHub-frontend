9010ca391174abb922ab7743fcc79210b784265e
Muhammed Rafi <rafikandathuvayal@gmail.com>
2024-10-10 19:49:41 +0530

    url changed

--- src/service/socketService.ts
+++ src/service/socketService.ts
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 class SocketService {
     socket;
     constructor() {
-        this.socket = io("https://hivehub.onrender.com");
+        this.socket = io("https://hivehub.shop");
     }
 }
 
